a1f7541d00e99eb157c9f1f7246e7d46dedd84da
Mauricio Mendes <mauricio.eletrica@gmail.com>
2023-09-26 15:23:01 +0200

    Update mytest.cpp

--- mytest.cpp
+++ mytest.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "mytest.h"
 
+using namespace std;
+
 int my_add( int a, int b ) {
+  cout << "my_add: : " << (a + b) << endl; 
   return a + b;
 }
 
 int my_sub( int a, int b ) {
+  cout << "my_sub: : " << (a - b) << endl; 
   return a - b;
 }
